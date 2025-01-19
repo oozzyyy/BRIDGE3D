824f69823590c1386ba3b3f675ecb9d8449de4ff
Tiunov Vladimir <tiunov2008.va@gmail.com>
2025-01-19 16:13:26 +0300

    fixes

--- bridge_project/settings.py
+++ bridge_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,11 +52,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'bridge_project.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
